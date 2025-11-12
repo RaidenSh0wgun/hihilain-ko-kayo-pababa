ef79b34e3da06fcaf825a437c9a87ffa144d7f7d
RaidenSh0wgun <markvincentcb@gmail.com>
2025-11-12 21:15:03 +0800

    Delete JavaScript directory

--- JavaScript/8Ball/main.js
+++
@@ -1,8 +0,0 @@
-let randomNumber = Math.random();
-console.log("Random number between 0 and 1:", randomNumber);
-
-let roundedNumber = Math.floor(randomNumber * 10);
-console.log("Random number between 0 and 9:", roundedNumber);
-
-let anotherRandomNumber = Math.floor(Math.random() * 100) + 1;
-console.log("Random number between 1 and 100:", anotherRandomNumber);
--- JavaScript/Dog years.js
+++
@@ -1,5 +0,0 @@
-let X = 18
-
-let Y = (X, 10.5) * 2 + (0, X - 2) * 4
-
-console.log("My name is Mark. I am " + X + " years old in human years which is " + Y + " in dog years.")
